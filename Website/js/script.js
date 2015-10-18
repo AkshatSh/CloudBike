43079a6322d2a95b731d5f2f929c8ea35f8ec2a3
Cedric Ith <cedric.ith1@gmail.com>
2015-10-18 05:01:58 -0700

    javascript

--- Website/js/script.js
+++ Website/js/script.js
@@ -1,27 +1,76 @@
 (function(){
 	window.onload = function() {
+		distanceGraph();
 		speedGraph();
 		googleMap();
 	}
 
-	function speedGraph () {
+	function distanceGraph () {
 		$(function () {
 		    $('#container').highcharts({
 		        title: {
-		            text: 'Monthly Average Temperature',
+		            text: 'Distance',
 		            x: -20 //center
 		        },
 		        subtitle: {
-		            text: 'Source: WorldClimate.com',
+		            text: '',
 		            x: -20
 		        },
 		        xAxis: {
-		            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-		                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+		            title: {
+		                text: 'Time (minutes)'
+		            },
+		            categories: ['5', '10', '15', '20', '25', '30',
+		                '35', '40', '45', '50', '55', '60']
 		        },
 		        yAxis: {
 		            title: {
-		                text: 'Temperature (°C)'
+		                text: 'Distance Traveled (feet)'
+		            },
+		            plotLines: [{
+		                value: 0,
+		                width: 1,
+		                color: '#9b59b6'
+		            }]
+		        },
+		        tooltip: {
+		            valueSuffix: '°C'
+		        },
+		        legend: {
+		            layout: 'vertical',
+		            align: 'right',
+		            verticalAlign: 'middle',
+		            borderWidth: 0
+		        },
+		        series: [{
+		            name: 'London',
+		            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+		        }]
+		    });
+		});
+	}
+
+	function speedGraph () {
+		$(function () {
+		    $('#container2').highcharts({
+		        title: {
+		            text: 'Speed',
+		            x: -20 //center
+		        },
+		        subtitle: {
+		            text: '',
+		            x: -20
+		        },
+		        xAxis: {
+		            title: {
+		                text: 'Time (minutes)'
+		            },
+		            categories: ['5', '10', '15', '20', '25', '30',
+		                '35', '40', '45', '50', '55', '60']
+		        },
+		        yAxis: {
+		            title: {
+		                text: 'Speed (feet/second)'
 		            },
 		            plotLines: [{
 		                value: 0,
@@ -40,21 +89,12 @@
 		        },
 		        series: [{
 		            name: 'Tokyo',
-		            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-		        }, {
-		            name: 'New York',
-		            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-		        }, {
-		            name: 'Berlin',
-		            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-		        }, {
-		            name: 'London',
-		            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+		            data: [17.0, 26.9, 29.5, 24.5, 18.2, 21.5, 25.2, 16.5, 13.3, 18.3, 13.9, 9.6]
 		        }]
 		    });
 		});
 	}
-	
+
 	function googleMap() {
 		// This example creates a 2-pixel-wide red polyline showing the path of William
 		// Kingsford Smith's first trans-Pacific flight between Oakland, CA, and
