90bbf93d158319fb0e17d18721a14c9f2e28c059
Joseph Zhong <joseph.zhong@hotmail.com>
2015-10-18 06:34:50 -0700

    reads data from arduino -- needs simultaneous between band and arduino

--- AndroidApp/CloudBikeBackend/app/src/main/java/com/example/joseph/cloudbikebackend/MainActivity.java
+++ AndroidApp/CloudBikeBackend/app/src/main/java/com/example/joseph/cloudbikebackend/MainActivity.java
@@ -6,6 +6,8 @@ import android.app.Activity;
 import android.util.Log;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class MainActivity extends Activity implements View.OnClickListener {
     private String TAG = "MainActivity";
     private int heartRate = 0;
@@ -16,8 +18,12 @@ public class MainActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         microsoftBandManager = new MicrosoftBandManager(this);
-
         bluetoothManager = new BluetoothManager();
+
+//        ArrayList<String> devicesConnected = bluetoothManager.getListOfPairedDevices();
+//        for(int i = 0; i < devicesConnected.size(); i++) {
+//            Log.i(TAG, devicesConnected.get(i));
+//        }
 
         new LongOperation().execute("");
     }
@@ -32,6 +38,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
         protected String doInBackground(String... params) {
             microsoftBandManager.connectToBand();
             microsoftBandManager.startListening();
+//            bluetoothManager.setupRecieve();
+//            bluetoothManager.connectInput();
 
             while(heartRate == 0) {
                 heartRate = microsoftBandManager.getHeartRate();
@@ -55,10 +63,6 @@ public class MainActivity extends Activity implements View.OnClickListener {
         @Override
         protected void onProgressUpdate(Void... values) {}
 
-
-        private void runInBackground() {
-
-        }
     }
 
 
