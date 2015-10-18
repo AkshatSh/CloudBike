eff73c6db278056b1b188f76ea2f068b7e79afe1
Cedric Ith <cedric.ith1@gmail.com>
2015-10-18 02:56:41 -0700

    everything

---
+++ Website/js/script.js
@@ -0,0 +1,88 @@
+(function(){
+	window.onload = function() {
+		speedGraph();
+		googleMap();
+	}
+
+	function speedGraph () {
+		$(function () {
+		    $('#container').highcharts({
+		        title: {
+		            text: 'Monthly Average Temperature',
+		            x: -20 //center
+		        },
+		        subtitle: {
+		            text: 'Source: WorldClimate.com',
+		            x: -20
+		        },
+		        xAxis: {
+		            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+		                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+		        },
+		        yAxis: {
+		            title: {
+		                text: 'Temperature (°C)'
+		            },
+		            plotLines: [{
+		                value: 0,
+		                width: 1,
+		                color: '#808080'
+		            }]
+		        },
+		        tooltip: {
+		            valueSuffix: '°C'
+		        },
+		        legend: {
+		            layout: 'vertical',
+		            align: 'right',
+		            verticalAlign: 'middle',
+		            borderWidth: 0
+		        },
+		        series: [{
+		            name: 'Tokyo',
+		            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+		        }, {
+		            name: 'New York',
+		            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+		        }, {
+		            name: 'Berlin',
+		            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+		        }, {
+		            name: 'London',
+		            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+		        }]
+		    });
+		});
+	}
+	
+	function googleMap() {
+		// This example creates a 2-pixel-wide red polyline showing the path of William
+		// Kingsford Smith's first trans-Pacific flight between Oakland, CA, and
+		// Brisbane, Australia.
+
+		function initMap() {
+		  var map = new google.maps.Map(document.getElementById('map'), {
+		    zoom: 3,
+		    center: {lat: 0, lng: -180},
+		    mapTypeId: google.maps.MapTypeId.TERRAIN
+		  });
+
+		  var flightPlanCoordinates = [
+		    {lat: 37.772, lng: -122.214},
+		    {lat: 21.291, lng: -157.821},
+		    {lat: -18.142, lng: 178.431},
+		    {lat: -27.467, lng: 153.027}
+		  ];
+		  var flightPath = new google.maps.Polyline({
+		    path: flightPlanCoordinates,
+		    geodesic: true,
+		    strokeColor: '#FF0000',
+		    strokeOpacity: 1.0,
+		    strokeWeight: 2
+		  });
+
+		  flightPath.setMap(map);
+		}
+	}
+	
+})();
