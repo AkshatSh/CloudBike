b3e3c52bd7cf3bc4f812c527601cabcb7eb0ff63
Cedric Ith <cedric.ith1@gmail.com>
2015-10-18 16:53:28 -0700

    tesetset

--- Website/js/cb_utils.js
+++ Website/js/cb_utils.js
@@ -1,4 +1,4 @@
-var BACKEND_HOST = "http://173.250.163.33:8080/api";
+var BACKEND_HOST = "http://cloudbike-backend.azurewebsites.net/api"; //http://173.250.163.33:8080/api
 var MENU_ENDPOINT = "/routes";
 var ROUTE_ENDPOINT = "/route";
 
@@ -13,20 +13,20 @@ function getMenuItems(callback) {
 function getMenuData() {
     getMenuItems(function () {
 	var data = JSON.parse(this.responseText);
-	return data;
+	console.log(data);
 	});
 }
 
 function getRouteData(routeid) {
     getRoute(routeid, function() {
 	    var data = JSON.parse(this.responseText);
-	    return data;
+	    console.log(data);
 	});
 }
 
-function getRoute(routeId, callBack) {
+function getRoute(routeId, callback) {
 	var ajax = new XMLHttpRequest();
 	ajax.onload = callback;
-	ajax.open("GET", BACKEND_HOST + MENU_ENDPOINT + ROUTE_ENDPOINT + "/" + routeId, true);
+	ajax.open("GET", BACKEND_HOST + ROUTE_ENDPOINT + "/" + routeId, true);
 	ajax.send();
 }
